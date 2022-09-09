b8e9820cb0840e011be8a00bf7f3f4f682a5eb70
arieltoniatto <ariel.toniatto@gmail.com>
2022-09-09 17:03:01 -0300

    tentando resolver erro de conflito

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 app.use('/products', productRouter);
+
 // não remova essa exportação, é para o avaliador funcionar.
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação
+
 module.exports = app;
