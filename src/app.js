4537eefaee5e7b2645acfb0eb741844d3d4862d5
arieltoniatto <ariel.toniatto@gmail.com>
2022-09-09 17:04:58 -0300

    erro persiste

--- src/app.js
+++ src/app.js
@@ -12,8 +12,7 @@ app.use(express.json());
 
 app.use('/products', productRouter);
 
-// não remova essa exportação, é para o avaliador funcionar.
+// não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
-
+// você deve usar o arquivo server.js para executar sua aplicação
 module.exports = app;
